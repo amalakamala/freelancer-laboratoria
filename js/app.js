2c6ea98968417af92581cf88a72f8473bd6ff63c
amalakamala <amalakamala.a@gmail.com>
2017-06-01 11:11:42 -0400

    portafolio model

--- js/app.js
+++ js/app.js
@@ -6,40 +6,54 @@
  	var elemetos = Array.from(document.getElementsByClassName("caja"));
  	var modal = document.getElementById("img-proyecto-modal");
 
- 	var cuerpoModal, close, icono;
+ 	var cuerpoModal, close, icono, hacheUno;
 
 
- 	elemetos.forEach(function(c){
+	elemetos.forEach(function(c){
  		c.addEventListener("click",function(){
  			modal.innerHTML="";
  			cuerpoModal = document.createElement("div");
- 			cuerpoModal.classList.add("modal-completo"); 			
+ 			cuerpoModal.classList.add("modal-completo");
+
+ 			cuerpoModal.innerHTML = c.innerHTML;
 
  			modal.appendChild(cuerpoModal);
- 			modal.classList.remove("hide");			
-			
- 			close = document.createElement("div"); 
- 			close.classList.add("close");
- 			icono = document.createTextNode("X");
- 			close.appendChild(icono);
+ 			modal.classList.remove("hide");
 
- 			modalDentro = document.createElement("div");
- 			modalDentro.classList.add("modal-dentro");
-
- 			hacheUno  = document.createElement("h1");
+ 			hacheUno = document.createElement("div");
  			hacheUnoT = document.createTextNode("PROJECT TITLE");
- 			hacheUno.appendChild("hacheUnoT");
+ 			hacheUno.classList.add("h-uno-modal")
+			hacheUno.appendChild(hacheUnoT);
+			modal.appendChild(hacheUno);
 
- 			linea = document.createElement("hr");
- 			linea.classList.add("star-primary");
+			estrella = document.createElement("hr");
+ 			estrella.classList.add("star-primary");
+ 			estrella.classList.add("estrella");
+			modal.appendChild(estrella);
 
- 			modalDentro.appendChild(hacheUno);
- 			modalDentro.appendChild(linea);
+			texto = document.createElement("p");
+			texto.classList.add("parrafo-modal");
+			textoT = document.createTextNode("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo excepturi, maxime illo natus id sit molestiae inventore cum, ipsum saepe iste. Illo impedit totam repellat natus, sint ducimus ea maxime. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo excepturi, maxime illo natus id sit molestiae inventore cum, ipsum saepe iste. Illo impedit totam repellat natus, sint ducimus ea maxime.");
+			texto.appendChild(textoT);
+			modal.appendChild(texto);	
 
- 			modalDentro.innerHTML = c.innerHTML;
+			botonN = document.createElement("a");
+			botonNT = document.createTextNode("submit");
+			botonN.setAttribute("href","#");
+			botonN.setAttribute("type","submit");
+			botonN.appendChild(botonNT);
+			botonN.classList.add("el-boton-modal");
+			modal.appendChild(botonN);
 
+ 			close = document.createElement("div"); 
+ 			close.classList.add("close-img");
+ 			icono = document.createElement("i");
+ 			icono.setAttribute("class","fa fa-times-circle");
+ 			icono.setAttribute("aria-hidden","true");
+
+ 			close.appendChild(icono);
  			modal.appendChild(close);
- 			modal.appendChild(modalDentro);
+			
 
 
  			close.addEventListener("click", function(){
